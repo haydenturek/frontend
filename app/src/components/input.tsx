6f8937d544ab538b7b539c96323d11923b887b45
rooster man <roostermanred@gmail.com>
2025-07-30 15:44:47 -0500

    styles

---
+++ app/src/components/input.tsx
@@ -0,0 +1,9 @@
+export default function Input() {
+  return (
+    <input
+      type="text"
+      className="backdrop-filter backdrop-blur-md bg-black/30 border border-gray-600 p-2 rounded-xl w-200 h-20 focus:outline-none focus:ring-0 p-6"
+      placeholder="Enter text here..."
+    />
+  )
+}
